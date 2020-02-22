a89435c495919b88745c56e74ca1e316c75594c9
oddcoder <ahmedsoliman@oddcoder.com>
2020-02-22 20:00:51 +0200

    Initial commit

---
+++ scripts/env.sh
@@ -0,0 +1,8 @@
+set +h
+LFS=/home/oddcoder/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-relibc
+PATH=/cross_tools/bin:/bin:/usr/bin
+export LFS_HOST="$(echo $MACHTYPE | sed "s/$(echo $MACHTYPE | cut -d- -f2)/cross/")"
+export LFS LC_ALL LFS_TGT PATH LFS_HOST
+
