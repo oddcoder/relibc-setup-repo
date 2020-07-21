ff6009acb33b0160157a8faff51a2dbf5da5f5c0
no name <no@mail>
2020-07-21 10:17:20 +0000

    add binutils stage2

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,7 +2,7 @@ set +h
 LFS=/home/oddcoder/lfs
 LC_ALL=POSIX
 LFS_TGT=$(uname -m)-lfs-linux-relibc
-PATH=/cross_tools/bin:/bin:/usr/bin
+export PATH=/cross_tools/bin:$PATH
 export LFS_HOST="$(echo $MACHTYPE | sed "s/$(echo $MACHTYPE | cut -d- -f2)/cross/")"
 export LFS LC_ALL LFS_TGT PATH LFS_HOST
 
